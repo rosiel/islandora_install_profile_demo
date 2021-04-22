1374f8172c6b9b1b968f43f55ffcfe60bbc24109
dannylamb <dlamb@islandora.ca>
2021-04-22 12:54:49 -0300

    Missing theme in composer.json

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-# Install Drupal
-drush si -y --account-name=admin --account-pass="${DRUPAL_DEFAULT_ACCOUNT_PASSWORD}"
-# Add admin role to admin
-drush user-add-role "administrator" admin
-drush en search_api_solr_defaults -y
-# Run support utilities
-source /etc/islandora/utilities.sh
-chmod +x /etc/islandora/site_init.sh && source /etc/islandora/site_init.sh  && for_all_sites provision_site
